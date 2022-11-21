20ddd0bac2ec4528f72344904f0efa5124b0f6dc
indry widiyani <indrywidiyani5@gmail.com>
2022-11-21 13:52:34 +0700

    menambahkan bilanganterbesar.py

---
+++ bilanganterbesar.py
@@ -0,0 +1,9 @@
+a = int(input("masukkan bilangan A: "))
+b = int(input("masukkan bilangan B: "))
+c = int(input("masukkan bilangan C: "))
+if a > b and a > c:
+  print('A yang terbesar')
+elif b > a and b > c:
+  print('B yang terbesar')
+else:
+  print('C yang terbesar')
